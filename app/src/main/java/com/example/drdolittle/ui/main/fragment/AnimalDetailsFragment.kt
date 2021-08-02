c690c26a6f868f91ac5b6178ae262bd38698492b
Joanderson <joandersongsantos@yahoo.com>
2021-08-01 21:08:46 -0300

    feat: implement data binding

--- app/src/main/java/com/example/drdolittle/ui/main/fragment/AnimalDetailsFragment.kt
+++ app/src/main/java/com/example/drdolittle/ui/main/fragment/AnimalDetailsFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.drdolittle.R
+import com.example.drdolittle.databinding.AnimalDetailsFragmentBinding
 import com.example.drdolittle.ui.main.viewmodel.AnimalDetailsViewModel
-import com.example.drdolittle.ui.main.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class AnimalDetailsFragment : Fragment() {
 
     private lateinit var viewModel: AnimalDetailsViewModel
+    private lateinit var binding : AnimalDetailsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +26,25 @@ class AnimalDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(AnimalDetailsViewModel::class.java)
+
+        binding = AnimalDetailsFragmentBinding.bind(view)
+        binding.animal = viewModel.getNextAnimal()
+
+        binding.btNext.setOnClickListener {
+            if (viewModel.hasNextAnimal()) {
+                binding.animal = viewModel.getNextAnimal()
+            } else {
+                Snackbar.make(view, "Você já viu todos os animais disponíveis! Espere chegar mais.", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        binding.btPrevious.setOnClickListener {
+            if (viewModel.hasPreviousAnimal()) {
+                binding.animal = viewModel.getPreviousAnimal()
+            } else {
+                Snackbar.make(view, "Este é o primeiro animal que você viu.", Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 
 }

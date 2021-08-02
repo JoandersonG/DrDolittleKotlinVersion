cec1bbc75ed1176e7eb9c8e6cb632656b7569387
Joanderson <joandersongsantos@yahoo.com>
2021-08-01 21:08:34 -0300

    feat: implement ViewModel

--- app/src/main/java/com/example/drdolittle/ui/main/viewmodel/AnimalDetailsViewModel.kt
+++ app/src/main/java/com/example/drdolittle/ui/main/viewmodel/AnimalDetailsViewModel.kt
@@ -1,7 +1,42 @@
 package com.example.drdolittle.ui.main.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.drdolittle.model.Animal
+import com.example.drdolittle.model.AnimalRepository
 
 class AnimalDetailsViewModel : ViewModel() {
-    //TODO: implementar viewmodel
+
+    private val repository : AnimalRepository = AnimalRepository()
+
+    private val animals : LiveData<List<Animal>> = repository.getAnimals()
+
+    private var index = -1
+
+    fun getNextAnimal(): Animal? {
+        animals.value?.let {
+            if (index+1 < it.size) {
+                return animals.value?.get(++index)
+            }
+        }
+        return null
+    }
+
+    fun hasNextAnimal(): Boolean {
+        animals.value?.let {
+            return index+1 < it.size
+        }
+        return false
+    }
+
+    fun getPreviousAnimal(): Animal? {
+        if (index-1 >= 0) {
+            return animals.value?.get(--index)
+        }
+        return null
+    }
+
+    fun hasPreviousAnimal(): Boolean {
+        return index-1 >= 0
+    }
 }
